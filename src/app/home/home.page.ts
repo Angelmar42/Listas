d557938559a9bd374fad0c4a002857d8bb13872c
Angelmar42 <angel.martinez5933@alumnos.udg.mx>
2022-04-19 13:20:04 -0500

    inicio

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+data: any;
+  constructor(private barcodeScanner: BarcodeScanner ) {}
+scan(){
+  this.barcodeScanner.scan().then(barcodeData => {
+    console.log('Barcode data', barcodeData);
+    this.data=barcodeData.text
+   }).catch(err => {
+       console.log('Error', err);
+   });
+}
 }
